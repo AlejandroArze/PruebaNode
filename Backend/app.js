c62d42bd4a21f58705355b640e0d0c67e28e0f57
fabianavillagra <fabianavillagra4@gmail.com>
2024-08-26 17:01:14 -0400

    Install express and connection db

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('postgres://postgres:postgres@localhost:5432/laboratorio_bd')
+
 
 app.get('/api/v1/', (req,res)=>{
     res.send("hello world");
@@ -9,3 +12,12 @@ app.get('/api/v1/', (req,res)=>{
 app.listen(port,()=>{
     console.log(`aplicacion esta usando ${port}`);
 });
+
+
+try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
+
